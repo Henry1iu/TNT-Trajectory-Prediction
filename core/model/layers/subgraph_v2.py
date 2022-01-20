15b4f6b786c0c5081deff77ec219e9565a9a00cc
jb <liu.jb.henry@gmail.com>
2022-01-20 14:34:29 +0800

    change the normalization dimension

--- core/model/layers/subgraph_v2.py
+++ core/model/layers/subgraph_v2.py
@@ -59,7 +59,7 @@ class SubGraph(nn.Module):
         x = out_data.x
 
         assert x.shape[0] % int(sub_data.time_step_len[0]) == 0
-        # return x / (x.norm(dim=0) + 1e-12)      # L2 normalization
+
         return F.normalize(x, p=2.0, dim=1)      # L2 normalization
 # %%
 
