e9662c122159b8e2c1f6b5c4d26b13b3c6c0f57e
LIU Jianbang <liu.jb.henry@gmail.com>
2022-01-27 20:02:43 +0800

    Update preprocessing.bash
    correct tag

--- scripts/preprocessing.bash
+++ scripts/preprocessing.bash
@@ -1,2 +1,2 @@
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
-python core/util/preprocessor/argoverse_preprocess_v2.py -root dataset/ -dest dataset
+python core/util/preprocessor/argoverse_preprocess_v2.py --root dataset/ --dest dataset
