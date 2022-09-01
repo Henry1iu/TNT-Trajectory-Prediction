fix bug in the rotation normalization

core/util/preprocessor/argoverse_preprocess_v3.py
@@ -151,7 +151,7 @@ class ArgoversePreprocessor(Preprocessor):
         tar_candts = self.lane_candidate_sampling(ctr_line_candts, self.LANE_WIDTH[city], viz=False)
 
         if self.split == "test":
-            tar_candts_gt, tar_offse_gt = np.zeros((tar_candts.shape[0], 1)), np.zeros((1, 2))
+            tar_candts_gt, tar_offse_gt = np.zeros((tar_candts.shape[0], 1)), np.zeros((2,))
         else:
             tar_candts_gt, tar_offse_gt = self.get_candidate_gt(tar_candts, agt_traj_fut[-1])
 

core/util/preprocessor/base.py
@@ -148,7 +148,7 @@ class Preprocessor(Dataset):
         """
         find the target candidate closest to the gt and output the one-hot ground truth
         :param target_candidate, (N, 2) candidates
-        :param gt_target, (1, 2) the coordinate of final target
+        :param gt_target, (2, ) the coordinate of final target
         """
         displacement = gt_target - target_candidate
         gt_index = np.argmin(np.power(displacement[:, 0], 2) + np.power(displacement[:, 1], 2))